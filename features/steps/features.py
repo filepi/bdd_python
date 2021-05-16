03dfcdd0dc3cc1d7d59b603e2fb5ae80f8273038
Felipe <felipe.barbosa@apple.com>
2021-05-16 12:16:43 -0300

    espaco

--- features/steps/features.py
+++ features/steps/features.py
@@ -11,6 +11,6 @@ STORE_INVENTORY = "store/inventory"
 @given('I get the store inventory')
 def get_request(context):
     r = requests.get(MAIN_URL + STORE_INVENTORY)
-    parsed = json.loads(r.text)#convert do Dictonary so we can indent
+    parsed = json.loads(r.text) #convert do Dictonary so we can indent
     print(json.dumps(parsed, indent=4, sort_keys=True))
     assert_that(r.status_code, equal_to(200))
