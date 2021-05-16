e65c9b922469c646b0c8f9665f973b9db4227a69
Felipe <felipe.barbosa@apple.com>
2021-05-16 12:14:14 -0300

    ident json

--- features/steps/features.py
+++ features/steps/features.py
@@ -2,18 +2,15 @@ from behave import given
 from hamcrest import assert_that, equal_to
 import requests
 import json
-from configuration import execute_frontend_test
 
 
-URL = "https://petstore.swagger.io/v2/pet/"
+MAIN_URL = "https://petstore.swagger.io/v2/"
+STORE_INVENTORY = "store/inventory"
 
-@given('I make a GET Request using ID {id}')
-def get_request(context, id):
-    r = requests.get(URL + '3')
+
+@given('I get the store inventory')
+def get_request(context):
+    r = requests.get(MAIN_URL + STORE_INVENTORY)
+    parsed = json.loads(r.text)#convert do Dictonary so we can indent
+    print(json.dumps(parsed, indent=4, sort_keys=True))
     assert_that(r.status_code, equal_to(200))
-
-
-def post_request():
-    r = requests.post('https://petstore.swagger.io/v2/pet/findByStatus?status=available')
-    print(json.dumps(r.json(), indent=4, sort_keys=True))
-
